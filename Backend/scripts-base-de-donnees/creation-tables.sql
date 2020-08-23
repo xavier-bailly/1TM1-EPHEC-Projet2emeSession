dcdbe06e7c2f73b11c5c40ecf605a673a6dbd339
xavier-bailly <64276071+xavier-bailly@users.noreply.github.com>
2020-08-23 12:24:57 +0200

    Update creation-tables.sql

--- Backend/scripts-base-de-donnees/creation-tables.sql
+++ Backend/scripts-base-de-donnees/creation-tables.sql
@@ -1,28 +1,20 @@
-	create table tbclient (
-		nomC char(30) not null,
-		prenomP char(30) not null,
-		nbcommande char(100),
-		commandeId INTEGER NOT NULL default autoincrement,
-		CONSTRAINT PK_nomC PRIMARY KEY (nomc),
-		CONSTRAINT FK_nbcommande FOREIGN KEY (nbcommande) REFERENCES tbcommande(nbcommande),
+	create table tbPrix (
+		prix char(30) not null,
+		prixId INTEGER NOT NULL default autoincrement,
+		CONSTRAINT PK_prixId PRIMARY KEY (prixId),
 		
 	);
 	
 	create table tbBiere (
 		biereId INTEGER NOT NULL default autoincrement,
 	    nomBiere char(30) not null,
-		prix char(60) not null,
-		prixId char(60) not null,
+		prixId integer not null,
 		CONSTRAINT PK_biereId PRIMARY KEY (biereId),
+		CONSTRAINT FK_prixId FOREIGN KEY (prixId) REFERENCES tbPrix(prixId),
 		
+
 	);
 	
-	create table tbPrix (
-		prix char(30) not null,
-		prixId INTEGER NOT NULL default autoincrement,
-		CONSTRAINT PK_prix PRIMARY KEY (prix),
-		CONSTRAINT FK_biereId FOREIGN KEY (biereId) REFERENCES tbBiere(biereId),
-	);
 		
 	create table tbCommande (
 		nbcommande char(100),
@@ -32,3 +24,12 @@
 		CONSTRAINT FK_biereId FOREIGN KEY (biereId) REFERENCES tbBiere(biereId),
 	);
 	
+	create table tbclient (
+		nomC char(30) not null,
+		prenomC char(30) not null,
+		nbCommande char(100),
+		commandeId INTEGER NOT NULL default autoincrement,
+		CONSTRAINT PK_nomC PRIMARY KEY (nomc),
+		CONSTRAINT FK_nbcommande FOREIGN KEY (nbcommande) REFERENCES tbcommande(nbcommande),
+		
+	);
